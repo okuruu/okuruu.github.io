46a76a3067fe1cf750ed68dbac558c13cf4c2d68
okuruu <Phantom_Ghost@outlook.co.id>
2023-08-13 08:19:33 +0700

    UD84 - Temporary

---
+++ _app/immutable/chunks/baseConfiguration.5e457b69.js
@@ -0,0 +1,1 @@
+const a={baseLink:"https://esdelfron.deabakery.co.id/api/UD84/"};export{a as b};
---
+++ _app/immutable/chunks/formatter.7f203817.js
@@ -0,0 +1,1 @@
+let r=new Intl.NumberFormat("id-ID",{style:"currency",currency:"IDR",maximumSignificantDigits:4});export{r};
