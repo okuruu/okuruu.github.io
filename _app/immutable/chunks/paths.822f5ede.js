a644a7a6acfe5761d0e20c4035777ba470b13545
okuruu <Phantom_Ghost@outlook.co.id>
2023-08-28 19:53:32 +0700

    Update POS UD84 Finale 💫

---
+++ _app/immutable/chunks/paths.822f5ede.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1k5oat4)==null?void 0:s.base)??"/okuruu.github.io";var t;const e=((t=globalThis.__sveltekit_1k5oat4)==null?void 0:t.assets)??a;export{e as a,a as b};
