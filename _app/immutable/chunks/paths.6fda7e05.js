35d4f3b3728abac8fed3418031b99b7d59077c35
okuruu <Phantom_Ghost@outlook.co.id>
2023-09-26 19:31:07 +0700

    Update Sak

---
+++ _app/immutable/chunks/paths.6fda7e05.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1q0v4ab)==null?void 0:s.base)??"/okuruu.github.io";var a;const e=((a=globalThis.__sveltekit_1q0v4ab)==null?void 0:a.assets)??t;export{e as a,t as b};
